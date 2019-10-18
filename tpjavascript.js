d599e34c5c0bc0f29394ab055733c8a407818c68
Celina Gijon <cgijon@MacBook-Pro-de-Celina.local>
2019-10-18 20:16:50 -0300

    subo los practicos 27-32

---
+++ tpjavascript.js
@@ -0,0 +1,67 @@
+// JS27 
+
+let numero1 = 5
+
+let numero2 = 4
+
+let resultado = numero1 + numero2
+
+console.log(resultado);
+
+let resultado2 = numero1 - numero2
+
+console.log(resultado2);
+
+let resultado3 = numero1 * numero2
+
+console.log(resultado3);
+
+let resultado4 = numero1 / numero2
+
+console.log(resultado4);
+
+let resultado5 = numero1 % numero2
+
+console.log(resultado5);
+
+// JS28
+
+let numero = 9
+console.log(numero*1, numero*2, numero*3, numero*4, numero*5, numero*6, numero*7, numero*8, numero*numero, numero*10)
+
+// JS29
+
+let lado = 10
+
+perimetroCuadrado = (lado * 4)
+
+console.log(perimetroCuadrado)
+
+// JS30
+
+let lados = 5
+
+areaCuadrado = (lados *5)
+
+console.log(areaCuadrado)
+
+// JS31
+
+let ladoUno = 10
+let ladoDos = 20
+let ladoTres = 5
+
+perimetroTriangulo = ladoUno + ladoDos + ladoTres
+
+console.log(perimetroTriangulo)
+
+// JS32
+
+let altura = 10
+let base = 4
+
+perimetro = base + (altura*2)
+area = base * altura
+
+console.log(area)
+console.log(perimetro)
