799b0cd280561c512df5c3e63548d36ba335edd5
Celina Gijon <cgijon@MacBook-Pro-de-Celina.local>
2019-10-18 20:57:46 -0300

    ejercicio 33

--- tpjavascript.js
+++ tpjavascript.js
@@ -65,3 +65,19 @@ area = base * altura
 
 console.log(area)
 console.log(perimetro)
+
+// JS33
+
+let cantidadDePersonas = 100
+
+cantidadDePersonas += 5
+
+console.log("Cantidad de personas:" + " " + cantidadDePersonas)
+
+cantidadDePersonas -= 3
+
+console.log("Cantidad de personas:" + " " + cantidadDePersonas)
+
+cantidadDePersonas *= 2
+
+console.log("Cantidad de personas:" + " " + cantidadDePersonas)
